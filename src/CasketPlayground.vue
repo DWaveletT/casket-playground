70f27cfc7b08bde89df3dc0f038909836ecaf3de
DWaveletT <dingwentao2004@126.com>
2024-05-29 14:58:20 +0800

    make progress

--- src/CasketPlayground.vue
+++ src/CasketPlayground.vue
@@ -9,7 +9,7 @@
                         <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" fill="currentColor"/>
                     </svg>
                 </h2>
-                <p class="subtitle">使用 Codemirror 6 和 Vue3，基于 Remark & Rehype 生态构建的最新一代 Markdown 编译器。<br><i>version 0.0.5</i></p>
+                <p class="subtitle">使用 Codemirror 6 和 Vue3，基于 Remark & Rehype 生态构建的最新一代 Markdown 编译器。<br><i>version 0.0.6</i></p>
 
             <div style="margin-bottom: 2em" />
 
@@ -627,12 +627,12 @@
 
 import { codes } from './codes';
 
-import { CasketStar, ToolLink, ToolTable, getDefaultPlugins, getDefaultToolbarL, getDefaultToolbarR, type Plugins, type Toolbar } from 'casket-star';
+import { CasketStar, ToolLink, ToolTable, getEmptyPlugins, getDefaultToolbarL, getDefaultToolbarR, markdown } from 'casket-star';
 
 import 'casket-star/themes/markdown/light.css';
 import 'casket-star/themes/casket/light.css';
 
-import { computed, onMounted, ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import remarkMath from './plugins/remark-math-luogu';
 import rehypeKatex from 'rehype-katex';
@@ -641,7 +641,7 @@ import rehypeHighlight from 'rehype-highlight';
 import remarkExtendedTable, { extendedTableHandlers } from 'remark-extended-table';
 import remarkDirective from 'remark-directive';
 import { LuoguMath } from './plugins/codemirror-luogu-math';
-import type { MarkdownConfig, MarkdownExtension } from '@lezer/markdown';
+import type { MarkdownExtension } from '@lezer/markdown';
 import remarkVideo from './plugins/remark-video';
 import remarkCallouts from './plugins/remark-callouts';
 import { ToolVideoLink } from './plugins/casket-video-link';
@@ -674,17 +674,18 @@ function config(name: string){
 }
 
 const plugins = computed(() => {
-    const plugins = getDefaultPlugins();
+    const plugins = getEmptyPlugins();
 
     plugins.codemirror = plugins.codemirror || [];
-    plugins.remark = [remarkGfm];
-    plugins.rehype = [];
+    plugins.remark = plugins.remark || [];
+    plugins.rehype = plugins.rehype || [];
+
+    plugins.remarkRehypeOptions = plugins.remarkRehypeOptions || {};
+
+    plugins.remark.push(remarkGfm);
 
     const markdownConfig: Record<string, unknown> = {};
-
     markdownConfig.extensions = [];
-
-    plugins.remarkRehypeOptions = {};
 
     if(listConfig.value.includes('html')){
         plugins.remarkRehypeOptions.allowDangerousHtml = true;
@@ -720,6 +721,8 @@ const plugins = computed(() => {
             plugins.remark.push(remarkCallouts);
         }
     }
+
+    plugins.codemirror.push(markdown(markdownConfig));
 
     return plugins;
 });
@@ -760,9 +763,21 @@ const toolbarl = computed(() => {
     }
 
     if(listConfig.value.includes('block')){
-        const i = toolbar.findIndex((g) => g.findIndex((t) => t.name === 'link') !== -1);
-        if(i !== -1){
-            toolbar[i].push(ToolBlock);
+        let exist = false;
+        for(let toolgroup of toolbar){
+            const i = toolgroup.findIndex((t) => t.name === 'block');
+            if(i !== -1){
+                exist = true;
+            }
+        }
+        if(exist === false){
+            for(let toolgroup of toolbar){
+                const i = toolgroup.findIndex((t) => t.name === 'link');
+                if(i !== -1){
+                    toolgroup.push(ToolBlock);
+                }
+            }
+
         }
     } else {
         for(let toolgroup of toolbar){
