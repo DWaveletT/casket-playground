18b5f0aec0cc7b021cfec47cb2f158a126189df9
DWaveletT <dingwentao2004@126.com>
2024-06-07 21:02:05 +0800

    add: i18n

--- src/CasketPlayground.vue
+++ src/CasketPlayground.vue
@@ -9,17 +9,18 @@
                         <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" fill="currentColor"/>
                     </svg>
                 </h2>
-                <p class="subtitle">使用 Codemirror 6 和 Vue3，基于 Remark & Rehype 生态构建的最新一代 Markdown 编译器。<br><i>version 0.0.12</i></p>
+                <p class="subtitle">使用 Codemirror 6 和 Vue3，基于 Remark & Rehype 生态构建的最新一代 Markdown 编译器。<br><i>version 0.0.14</i></p>
 
             <div style="margin-bottom: 2em" />
 
             <casket-star
-                v-model="value" lang="zh_CN"
+                v-model="value"
                 :plugins="plugins"
                 :toolbarl="toolbarl"
                 :toolbarr="toolbarr"
                 :key="updateTime"
                 :upload="upload"
+                :i18n="lang === 'zh_CN' ? zhCN : enUS"
                 />
         </div>
 
@@ -33,10 +34,10 @@
                         选择语言
                     </div>
                     <div class="items">
-                        <div class="item">
+                        <div class="item" :class="{ selected: lang === 'zh_CN'}" @click="lang = 'zh_CN'">
                             简体中文
                         </div>
-                        <div class="item">
+                        <div class="item" :class="{ selected: lang === 'en_US'}" @click="lang = 'en_US'">
                             English
                         </div>
                     </div>
@@ -177,9 +178,6 @@
             <p>Codemirror 的外观通过使用 Codemirror 插件来实现。</p>
             
             <i>TODO</i>
-
-            <m-editor></m-editor>
-
 
             <h2 id="detail" class="section-title">技术细节<div class="decoration" /></h2>
 
@@ -640,6 +638,8 @@ import {
     getDefaultToolbarR, 
     markdown,
     markdownLanguage,
+    zhCN,
+    enUS,
     type Plugins,
     type Uploader
 } from 'casket-star';
@@ -647,7 +647,7 @@ import {
 import 'casket-star/themes/markdown/light.css';
 import 'casket-star/themes/casket/light.css';
 
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 import {
     remarkMath,
@@ -671,6 +671,7 @@ import rehypeKatex from 'rehype-katex';
 import remarkExtendedTable, { extendedTableHandlers } from 'remark-extended-table';
 
 import type { MarkdownExtension } from '@lezer/markdown';
+import type { Plugin } from 'unified';
 
 const value = ref('');
 
@@ -687,6 +688,12 @@ const options = {
     'table': '合并单元格',
     'block': '样式化块体',
 }
+
+const lang = ref('zh_CN');
+
+watch(lang, () => {
+    updateTime.value = new Date().getTime();
+})
 
 function config(name: string){
     if(listConfig.value.includes(name)){
@@ -1032,8 +1039,14 @@ h1, h2, h3, h4, h5, h6 {
                     padding: 0.5em 1em;
                     background-color: white;
 
+                    cursor: pointer;
+
                     &:not(:last-child){
                         border-bottom: 1px solid $casket-color;
+                    }
+
+                    &.selected {
+                        background-color: $casket-nv-color;
                     }
                 }
             }
